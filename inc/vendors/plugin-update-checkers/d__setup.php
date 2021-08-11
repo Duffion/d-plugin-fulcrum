da94e931089e78479dc557d3a1b33e511d343eea
Chris 'Duffs' Crevling <christopher.crevling@gmail.com>
2021-08-11 01:52:07 -0700

    Updating fixes for git_updater

--- inc/vendors/plugin-update-checkers/d__setup.php
+++ inc/vendors/plugin-update-checkers/d__setup.php
@@ -1,21 +1,22 @@
 <?php
 
-require 'plugin-update-checker.php';
+// require 'plugin-update-checker.php';
+// global $d_plugin_dirs;
 
-$config = [
-    'git' => 'https://github.com/Duffion/d-plugin-fulcrum',
-    'target_branch' => 'production'
-];
+// $config = [
+//     'git' => 'https://github.com/Duffion/d-plugin-fulcrum',
+//     'target_branch' => 'production'
+// ];
 
 
-$update_checker = Puc_v4_Factory::buildUpdateChecker(
-    $config['git'],
-    __FILE__,
-    'fulcrum'
-);
+// $update_checker = Puc_v4_Factory::buildUpdateChecker(
+//     $config['git'],
+//     __FILE__,
+//     'fulcrum'
+// );
 
-//Set the branch that contains the stable release.
-$update_checker->setBranch($config['target_branch']);
+// //Set the branch that contains the stable release.
+// $update_checker->setBranch($config['target_branch']);
 
 //Optional: If you're using a private repository, specify the access token like this:
 // $update_checker->setAuthentication($config['auth_token']);
